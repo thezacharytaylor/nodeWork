297382210222619e3b97548ba963888e34743840
Zach <zacharytaylor@tutamail.com>
2019-07-30 15:19:43 -0500

    Trying Node built-in modules, FS in particular

--- firstApp/app.js
+++ firstApp/app.js
@@ -1,3 +1,9 @@
 const logger = require('./logger');
+const fs = require('fs');
 
 logger.log();
+
+fs.readdir('./', function(err, files) {
+    if (err) console.log('Error', err);
+    else console.log(files);
+})
