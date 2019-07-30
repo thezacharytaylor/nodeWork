a20afcc6b200cab24ce1ca8ead86466892980614
Zach <zacharytaylor@tutamail.com>
2019-07-30 15:48:16 -0500

    Event experimentation

--- firstApp/app.js
+++ firstApp/app.js
@@ -1,9 +1,15 @@
 const logger = require('./logger');
 const fs = require('fs');
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
 
-logger.log();
+//logger.log();
 
-fs.readdir('./', function(err, files) {
-    if (err) console.log('Error', err);
-    else console.log(files);
-})
+// fs.readdir('./', function(err, files) {
+//     if (err) console.log('Error', err);
+//     else console.log(files);
+// })
+
+emitter.on('messageLogging', (arg) => { console.log('Message logging', arg); })
+
+emitter.emit('messageLogging', { message: 'Message is logging on Port 8.8.8.8' });
