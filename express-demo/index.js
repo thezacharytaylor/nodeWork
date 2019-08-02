0f5aa993d5609fae52943d5313bc2e56ccaa14bb
Zach <zacharytaylor@tutamail.com>
2019-08-02 14:13:09 -0500

    More express work
    Environment variable, route parms, HTTP Get & Post requests.

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+
+const courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
 
 app.get('/', (req, res) => {
-    res.send(['Hello World']);
+    res.send('Hello World!!');
 });
 
-app.listen(3000, () => { console.log('Listening on Port 3000'); });
+app.get('/api/courses', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    res.send([1, 2, 3]);
+});
+
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Course ID Not found');
+    res.send(course);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => { console.log(`Listening on Port ${port}`); });
