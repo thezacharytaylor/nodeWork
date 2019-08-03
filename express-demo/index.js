e6e54bdc208f693730d60b57316615c9297f1941
Zach <zacharytaylor@tutamail.com>
2019-08-03 12:17:31 -0500

    Joi Working

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,11 +1,13 @@
+const Joi = require('@hapi/joi');
 const express = require('express');
 const app = express();
+
 app.use(express.json());
 
 const courses = [
-    { id: 1, name: 'course1' },
-    { id: 2, name: 'course2' },
-    { id: 3, name: 'course3' }
+    { id: 1, name: "course1" },
+    { id: 2, name: "course2" },
+    { id: 3, name: "course3" }
 ];
 
 app.get('/', (req, res) => {
@@ -24,6 +26,14 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+    const schema = { name: Joi.string().min(3).required() };
+
+    const result = Joi.validate(req.body, schema);
+    if(result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
