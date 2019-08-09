e3d6719e92dea5e973e86903a29adcc943d441e7
Zach <zacharytaylor@tutamail.com>
2019-08-08 20:46:44 -0500

    Put completion && deletion progress.

--- express-demo/vidly.js
+++ express-demo/vidly.js
@@ -43,9 +43,28 @@ app.post('/api/genres', (req, res) => {
 });
 
 // Update aka Put
-app.put('/api/genres', );
+app.put('/api/genres/:id', (req, res) => {
+    //Get genre
+    const genre = genres.find(c => c.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('404: Genre Not Found');
 
-// Delete aka ..
+    //Validation of new name
+    const { error } = validateGenre(req.body.name);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //Change name && show the new genre
+    genre.name = req.body.name; 
+    res.send(genre);
+});
+
+// Delete aka .. delete
+app.delete('/api/genres/:id', (req, res) => {
+    // Get genre
+    const genre 
+
+    // Splice it (need a future fix that doesn't mutate)
+});
+
 
 function validateGenre(genre) {
     const schema = { name: Joi.string().min(3).required() }
